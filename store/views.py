67e780bb97ca05c6d7726eacc887f1abf9331bc4
Aziguy <monsieur_hernandez@hotmail.com>
2021-10-14 14:11:50 +0200

    Setting up of search feature

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from . models import Product
 from category. models import Category
+from django.db.models import Q
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
+
 def store(request, category_slug=None):
     categories = None
     products = None
@@ -41,3 +43,15 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
